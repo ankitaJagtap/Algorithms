a0fa74cc80d652ed08ad906b66d3c2d5ba3afc6b
ankitajagtap04@gmail.com <ankitajagtap04@gmail.com>
2024-03-07 23:24:52 +0530

    Algorithms

---
+++ DFS.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * CreateGraph
+ */
+public class DFS {
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 2));
+        graph[1].add(new Edge(1, 3));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+        graph[2].add(new Edge(2, 3));
+
+        graph[3].add(new Edge(3, 2));
+        graph[3].add(new Edge(3, 1));
+
+    }
+
+    public static void dfs(ArrayList<Edge> graph[], int curr, boolean vis[]) {
+        System.out.print(curr + " ");
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (vis[e.dest] == false) {
+                dfs(graph, e.dest, vis);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        boolean vis[] = new boolean[V];
+        createGraph(graph);
+        dfs(graph, 0, vis);
+
+    }
+}
